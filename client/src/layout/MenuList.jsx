b14a55468fdbe3be66e0bb05e578b3d1cd46cadd
Md. Mazharul Islam <mazharul.saurav255@gmail.com>
2024-06-26 02:10:33 +0600

    Theme dark22:16
    Theme dark22:16

--- client/src/layout/MenuList.jsx
+++ client/src/layout/MenuList.jsx
@@ -7,18 +7,22 @@ import {
     PayCircleOutlined,
     SettingOutlined
 } from '@ant-design/icons';
-const MenuList = () => {
+const MenuList = ({darkTheme}) => {
     return (
-        <Menu theme="dark">
+        <Menu theme={darkTheme ? 'dark' : 'light'} mode="inline" className='menu-bar'>
             <Menu.Item key="home" icon={<HomeOutlined />}>
                 Home
             </Menu.Item>
             <Menu.Item key="activity" icon={<AppstoreAddOutlined />}>
                 Activity
             </Menu.Item>
-            <Menu.SubMenu key="subtasks" icon={<BarsOutlined/>} title="Tasks">
+            <Menu.SubMenu key="subtasks 1" icon={<BarsOutlined/>} title="Tasks">
                 <Menu.Item key="task 1"> Task 1</Menu.Item>
                 <Menu.Item key="task 2"> Task 2</Menu.Item>
+                <Menu.SubMenu key="subtasks 2" icon={<BarsOutlined/>} title="SubTask">
+                    <Menu.Item key="Subtask 1"> Task 1</Menu.Item>
+                    <Menu.Item key="Subtask 2"> Task 2</Menu.Item>
+                </Menu.SubMenu>
             </Menu.SubMenu>
             <Menu.Item key="progress" icon={<AreaChartOutlined />}>
                 Progress
--- client/src/layout/SideNavbar.jsx
+++ client/src/layout/SideNavbar.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Layout} from 'antd'
 import Logo from "./Logo.jsx";
 import MenuList from "./MenuList.jsx";
+import ToggleThemeButton from "./ToggleThemeButton.jsx";
 
 const {Header,Sider} = Layout
 
 const SideNavbar = () => {
+    const [darkTheme,setDarkTheme] = useState(true);
+
+    const toggleTheme = () => {
+        setDarkTheme(!darkTheme)
+    }
+
     return (
         <Layout>
-            <Sider className="sidebar">
+            <Sider theme={darkTheme ? 'dark' : 'light'} className="sidebar">
                 <Logo />
-                <MenuList />
+                <MenuList darkTheme = {darkTheme}/>
+                <ToggleThemeButton darkTheme={darkTheme}
+                toggleTheme={toggleTheme}/>
             </Sider>
         </Layout>
     );
